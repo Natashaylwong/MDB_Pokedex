0fb4fe138bc1a76391e8c16deacae19ff2f2ada2
Natasha Wong <natashaylwong@berkeley.edu>
2018-02-15 18:09:32 -0800

    Updating scrolling and start screen

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     var scView:UIScrollView!
     let buttonPadding:CGFloat = 10
     var xOffset:CGFloat = 10
+    var filtered: Array<String>!
+    var types = ["Bug", "Grass", "Dark", "Dragon","Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Physic", "Rock", "Steel", "Water"]
 
 
     override func viewDidLoad() {
@@ -21,28 +23,30 @@ class ViewController: UIViewController {
     }
     
     func setupFilters() {
-        scView = UIScrollView(frame: CGRect(x: 0, y: 120, width: view.bounds.width, height: 50))
+        
+        scView = UIScrollView(frame: CGRect(x: 0, y: 120, width: view.bounds.width, height: 90))
         view.addSubview(scView)
         
-        scView.backgroundColor = UIColor.blue
+        scView.backgroundColor = .white
         scView.translatesAutoresizingMaskIntoConstraints = false
         
-        for i in 0 ... 10 {
+        for i in 0 ... 17 {
             let button = UIButton()
             button.tag = i
-            button.backgroundColor = UIColor.darkGray
-            button.setTitle("/(i)", for: .normal)
-            //button.addTarget(self, action: #selector(btnTouch), for: UIControlEvents.touchUpInside)
+            button.setImage(UIImage(named: "\(i).png"), for: .normal)
+            button.addTarget(self, action: #selector(typeButtonTouched), for: UIControlEvents.touchUpInside)
             
-            button.frame = CGRect(x: xOffset, y: CGFloat(buttonPadding), width: 70, height: 30)
+            button.frame = CGRect(x: xOffset, y: CGFloat(buttonPadding), width: 70, height: 70)
             
             xOffset = xOffset + CGFloat(buttonPadding) + button.frame.size.width
             scView.addSubview(button)
-            
-            
         }
-        
         scView.contentSize = CGSize(width: xOffset, height: scView.frame.height)
+        
+    }
+    
+    func typeButtonTouched() {
+        filtered.append(types[])
     }
 }
 
