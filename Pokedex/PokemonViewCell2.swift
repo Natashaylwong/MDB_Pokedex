3ed2541ce95ff7fc4de41c2a8d8e63f3e3500b50
Mudabbir Khan <m.khan@berkeley.edu>
2018-02-16 14:00:22 -0800

    almost done

--- Pokedex/PokemonViewCell2.swift
+++ Pokedex/PokemonViewCell2.swift
@@ -9,45 +9,46 @@
 import UIKit
 
 protocol PokemonViewCell2Delegate {
-    func changeColorOfButton2(forCell: PokemonViewCell2)
+    func listButton(forCell: PokemonViewCell2)
 }
 
 class PokemonViewCell2: UITableViewCell {
     
     var pokemonImageView: UIImageView!
     var cellLabel: UILabel!
-    var cellButton: UIButton!
+//    var cellButton: UIButton!
     var delegate: PokemonViewCell2Delegate? = nil
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        cellButton = UIButton(frame: contentView.frame)
-        //        cellButton.backgroundColor = UIColor.green
-        cellButton.layer.borderColor = UIColor.black.cgColor
-        cellButton.layer.borderWidth = 3
-        cellButton.layer.cornerRadius = 3
-        cellButton.clipsToBounds = true
-        cellButton.addTarget(self, action: #selector(changeColor), for: .touchUpInside)
-        //        cellButton.contentVerticalAlignment = UIControlContentVerticalAlignment.bottom
-        contentView.addSubview(cellButton)
-        cellLabel = UILabel(frame: CGRect(x: contentView.frame.maxX - 100, y: contentView.frame.midY, width: contentView.frame.width - 100, height: 20))
-        cellLabel.font = UIFont(name: "Pokemon Classic", size: 7)
+//        cellButton = UIButton(frame: contentView.frame)
+//        //        cellButton.backgroundColor = UIColor.green
+//        cellButton.layer.borderColor = UIColor.black.cgColor
+//        cellButton.layer.borderWidth = 3
+//        cellButton.layer.cornerRadius = 3
+//        cellButton.clipsToBounds = true
+//        cellButton.addTarget(self, action: #selector(changeColor), for: .touchUpInside)
+//        //        cellButton.contentVerticalAlignment = UIControlContentVerticalAlignment.bottom
+//        contentView.addSubview(cellButton)
+        cellLabel = UILabel(frame: CGRect(x: 80, y: contentView.frame.midY - 10, width: 200, height: 20))
+        cellLabel.font = UIFont(name: "Pokemon Classic", size: 10)
         contentView.addSubview(cellLabel)
-        pokemonImageView = UIImageView(frame: CGRect(x: 10, y: contentView.frame.midY, width: 100, height: contentView.frame.height - 10))
-        pokemonImageView.contentMode = .scaleToFill
+        pokemonImageView = UIImageView(frame: CGRect(x: 15, y: contentView.frame.minY + 5, width: 40, height: contentView.frame.height - 10))
+        pokemonImageView.contentMode = .scaleAspectFit
         pokemonImageView.clipsToBounds = true
-        contentView.addSubview(pokemonImageView) //Remember to add UI elements to the contentView, not the cell itself
+        contentView.addSubview(pokemonImageView)
+        //Remember to add UI elements to the contentView, not the cell itself
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
     
-    @objc func changeColor(sender: UIButton) {
-        print("changing color")
-        delegate?.changeColorOfButton2(forCell: self)
-    }
+//    @objc func changeColor(sender: UIButton) {
+//        print("changing color")
+//        delegate?.changeColorOfButton2(forCell: self)
+//    }
 
 }
